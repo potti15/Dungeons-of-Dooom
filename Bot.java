61be2d16595ce8a2951adfc58cf7cd64546b3265
Harry Pottinger <harryMac@138-38-193-104.resnet.bath.ac.uk>
2015-02-25 11:25:04 +0000

    Improved AI

--- Bot.java
+++ Bot.java
@@ -1,98 +1,122 @@
 
 public class Bot {
-	
+
 	private static GameLogic gameLogic;
-	
+
 	public static void main(String[] args) {
-		
+
 		gameLogic = new GameLogic();
 		System.out.println("Bot will play");
 		gameLogic.setupGame();
-		
+
 		while (!gameLogic.checkFinished()) {
-			
+
 			makeMove();
-			
+
 		}
-		
+
 		System.out.println("Finished.");
-		
+
 	}
-	
+
 	private static void makeMove() {
-		
+
 		gameLogic.pickUp();
 		char[][] fieldOfView = gameLogic.look();
 		char nextDirection;
 		nextDirection = checkForGold(fieldOfView);
-		
+
 		if (nextDirection == ' ') {
-			
+
 			nextDirection = randomDirection();
-		
+
 		}
-		
+
 		gameLogic.printMap();
 		gameLogic.move(String.valueOf(nextDirection));
-		
+
 	}
-	
+
+	private static char checkForImmediateGold(char[][] fieldOfView) {
+
+		char immediateDirection = ' ';
+
+		if (fieldOfView[2][1] == 'G') {
+
+			immediateDirection = 'W';
+
+		} else if (fieldOfView[2][3] == 'G') {
+
+			immediateDirection = 'E';
+
+		} else if (fieldOfView[1][2] == 'G') {
+
+			immediateDirection = 'N';
+
+		} else if (fieldOfView[3][2] == 'G') {
+
+			immediateDirection = 'S;'
+
+		}
+
+	}
+
 	private static char checkForGold(char[][] fieldOfView) {
-		
+
 		char directionOfGold = ' ';
-		
+
 		for (int i = 0; i < 5; i++) {
-			
+
 			for (int j = 0; j < 5; j++) {
-				
+
 				if (fieldOfView[i][j] == 'G') {
-					
+
 					directionOfGold = findRelativeDirection(i, j, 2, 2);
 					break;
-					
+
 				}
-				
+
 			}
-			
+
 		}
-		
+
 		return directionOfGold;
-		
+
 	}
-	
+
 	private static char findRelativeDirection(int yObject, int xObject, int xSubject, int ySubject) {
-		
+
 		char direction = ' ';
-		
+
 		if ((xObject == xSubject) && (yObject < ySubject)) {
-			
+
 			direction = 'N';
-			
+
 		} else if ((xObject == xSubject) && (yObject > ySubject)) {
-			
+
 			direction = 'S';
-			
+
 		} else if (xObject < xSubject) {
-			
+
 			direction = 'W';
-			
+
 		} else if (xObject > xSubject) {
-			
+
 			direction = 'E';
-			
+
 		}
-		
+
 		return direction;
-		
+
 	}
-	
+
 	private static char randomDirection() {
-		
+
 		char direction = ' ';
 		int directionInt = (int) Math.floor(Math.random() * 4);
-		
+
 		switch (directionInt) {
-		
+
 			case 0: direction = 'N';
 					break;
 			case 1: direction = 'E';
@@ -101,11 +125,11 @@ public class Bot {
 					break;
 			case 3: direction = 'W';
 					break;
-		
+
 		}
-		
+
 		return direction;
-		
+
 	}
 
 }
